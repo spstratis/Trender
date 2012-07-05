d36c814dfb8ceb7ff5b49bb8ad9eaaccb671a2f7
Steven Stratis <spstratis@gmail.com>
2012-07-05 19:49:30 -0400

    Added and filled hashtag controller with an array of three names.

--- Trender/TRHashtagListViewController.h
+++ Trender/TRHashtagListViewController.h
@@ -10,10 +10,10 @@
 
 #pragma mark - Interface
 
-// TODO: create hash tag array property
+// TODO: create hash tag array property (done)
+// TODO: init array with hash tags of presidents (done)
 
-// TODO: init array with hash tags of presidents
-// TODO: wire with number of columsn, number of rows, cell for index
+// TODO: wire with number of columsn, number of rows, cell for index (in progress)
 @interface TRHashtagListViewController : UITableViewController
 
 @property (nonatomic, strong) NSArray *hashtags;
--- Trender/TRHashtagListViewController.m
+++ Trender/TRHashtagListViewController.m
@@ -12,10 +12,82 @@
 
 @interface TRHashtagListViewController ()
 
+- (UITableViewCell *)configureFavoriteCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
+
 @end
 
 #pragma mark - Implementation
 
 @implementation TRHashtagListViewController
+
+//synth the hash tag from the header
+@synthesize hashtags;
+
+//if view is not laoded
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+    if (![self isViewLoaded]) {
+        self.hashtags = nil;
+    }
+}
+
+//check orientation
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
+    UIDevice *device = [UIDevice currentDevice];
+    
+    if (device.userInterfaceIdiom == UIUserInterfaceIdiomPhone) {
+        return (toInterfaceOrientation == UIInterfaceOrientationPortrait);
+    }
+    else {
+        return YES;
+    }
+}
+
+   //check if view did load 
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    //create and populate array with pres'
+    NSMutableArray *array = [[NSMutableArray alloc] initWithCapacity:3];
+    [array addObject:@"#Ron Paul"];
+    [array addObject:@"#Mitt Romney"];
+    [array addObject:@"#Barak Obama"];
+    
+    //set @property hashtags to an array 
+    self.hashtags = array;
+}
+
+-(void)viewDidUnload {
+    [super viewDidUnload];
+}
+
+#pragma mark - Instance Method
+
+//create the cell
+- (UITableViewCell *)configureFavoriteCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath{
+    cell.textLabel.text = [self.hashtags objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - protocal methods - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *identifier = @"TRHashtagCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    return [self configureFavoriteCell:cell forRowAtIndexPath:indexPath];
+}
+
+/*
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.hashtags count];
+}
+*/
 
 @end
