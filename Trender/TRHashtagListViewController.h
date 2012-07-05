ed6fb7bfa8e1de882248769e1ffd345778bb0e3d
Steven Stratis <spstratis@gmail.com>
2012-07-05 18:56:50 -0400

    added property

--- Trender/TRHashtagListViewController.h
+++ Trender/TRHashtagListViewController.h
@@ -11,8 +11,11 @@
 #pragma mark - Interface
 
 // TODO: create hash tag array property
+
 // TODO: init array with hash tags of presidents
 // TODO: wire with number of columsn, number of rows, cell for index
 @interface TRHashtagListViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *hashtags;
 
 @end
