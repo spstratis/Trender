f9950fef0709dedd3d77756796abaa0b93ce4526
Michael Reneer <michaelreneer@gmail.com>
2013-01-09 15:24:42 -0500

    Updated copyright.

--- README.markdown
+++ README.markdown
@@ -4,4 +4,4 @@ Trender
 License
 ----------
 
-Copyright (c) 2013 Trender. See LICENSE for details.
+Copyright (c) 2012-2013 Trender. See LICENSE for details.
--- Trender/TRFavoriteListViewController.m
+++ Trender/TRFavoriteListViewController.m
@@ -3,7 +3,7 @@
 //  Trender
 //
 //  Created by Michael Reneer on 7/3/12.
-//  Copyright (c) 2013 Trender. All rights reserved.
+//  Copyright (c) 2012 Trender. All rights reserved.
 //
 
 #import "TRFavorite.h"
